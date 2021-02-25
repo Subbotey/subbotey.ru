1f4735e3e4fbd47ed066421b18150a0ba751b7de
Subbotey <subbotey@yandex.ru>
2021-02-25 10:26:13 +0300

    Del console

--- public_html/console.php
+++
@@ -1,9 +0,0 @@
-<?php
-function console_log($data)
-{
-    if (is_array($data) || is_object($data)) {
-        echo("<script>console.log('php_array: ".json_encode($data)."');</script>");
-    } else {
-        echo("<script>console.log('php_string: ".$data."');</script>");
-    }
-}
