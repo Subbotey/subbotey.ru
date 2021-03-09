20ecfdb13f32790d08abd5ab8fccd3699304fa1c
Subbotey <subbotey@yandex.ru>
2021-03-09 12:04:10 +0300

    Sin

--- public_html/hello.php
+++ public_html/hello.php
@@ -1,3 +1,34 @@
 <?php
 
-echo 'Hello';
+require 'vendor/autoload.php';
+
+use \PhpOffice\PhpSpreadsheet\Shared\Date;
+
+$file = 'data.xlsx'; // файл для получения данных
+$excel = \PhpOffice\PhpSpreadsheet\IOFactory::load($file); // подключить Excel-файл
+
+$excel->setActiveSheetIndex(0); // получить данные из указанного листа
+$sheet = $excel->getActiveSheet();
+
+// формирование html-кода с данными
+$html = '<table>';
+foreach ($sheet->getRowIterator() as $row) {
+    $html .= '<tr>';
+    $cellIterator = $row->getCellIterator();
+    foreach ($cellIterator as $cell) {
+        // значение текущей ячейки
+        $value = $cell->getCalculatedValue();
+
+        // если дата, то преобразовать в формат PHP
+        if (Date::isDateTime($cell)) {
+            $value = date('d.m.Y', Date::excelToTimestamp($cell->getValue()));
+        }
+
+        $html .= '<td>'.$value.'</td>';
+    }
+    $html .= '<tr>';
+}
+$html .= '</table>';
+
+// вывод данных
+echo $html;
--- public_html/index.php
+++ public_html/index.php
@@ -1,12 +1,10 @@
 <?php
 
-error_reporting(E_ALL);
-
 include_once 'datePars.php';
 
 require_once __DIR__.'./src/SimpleXLSX.php';
 
-if ($xlsx = SimpleXLSX::parse('data.xlsx')) {
+if ($xlsx = SimpleXLSX::parse('kartochka.xlsx')) {
     $header_values = $rows = [];
     foreach ($xlsx->rows() as $k => $r) {
         if ($k === 0) {
@@ -16,60 +14,65 @@ if ($xlsx = SimpleXLSX::parse('data.xlsx')) {
         $rows[] = array_combine($header_values, $r);
     }
 }
+
+// $unix_time = filemtime('kartochka.xlsx');
+
+// echo date('d.m.Y', $unix_time);
 echo "<pre>";
 print_r($rows);
 echo "</pre>";
 
+// !
 
-$rows = array_map(function (&$tag) {
-    return array(
-        'level' => $tag['Уровень_структуры'],
-        'name' => $tag['Название'],
-        'fromDate' => $tag['Начало'],
-        'toDate' => $tag['Окончание'],
-        'leader' => $tag['Руководитель'],
-        'parent' => $tag['Предшественники'],
-        'percent' => $tag['Процент_завершения'],
-        'cost' => $tag['Затраты'],
-        'actualCost' => $tag['Фактические_затраты'],
-        'url' => $tag['Адрес_гиперссылки']
-    );
-}, $rows);
+// $rows = array_map(function (&$tag) {
+//     return array(
+//         'level' => $tag['Уровень_структуры'],
+//         'name' => $tag['Название'],
+//         'fromDate' => $tag['Начало'],
+//         'toDate' => $tag['Окончание'],
+//         'leader' => $tag['Руководитель'],
+//         'parent' => $tag['Предшественники'],
+//         'percent' => $tag['Процент_завершения'],
+//         'cost' => $tag['Затраты'],
+//         'actualCost' => $tag['Фактические_затраты'],
+//         'url' => $tag['Адрес_гиперссылки']
+//     );
+// }, $rows);
 
-$sortArr = [];
-$levelOne = [];
-$levelTwo = [];
+// $sortArr = [];
+// $levelOne = [];
+// $levelTwo = [];
 
-for ($i=0; $i < count($rows); $i++) {
-    if ($rows[$i]['level'] !== '0') {
-        $rows[$i]['percent'] = (float)$rows[$i]['percent'];
-        $rows[$i]['percent'] = round($rows[$i]['percent'], 2);
-        $rows[$i]['percent'] *= 100;
-        $rows[$i]['cost'] = (float)$rows[$i]['cost'];
-        $rows[$i]['cost'] = round($rows[$i]['cost'], 2);
-        $rows[$i]['actualCost'] = (float)$rows[$i]['actualCost'];
-        $rows[$i]['actualCost'] = round($rows[$i]['actualCost'], 2);
-        $rows[$i]['fromDate'] = datePars($rows[$i]['fromDate']);
-        $rows[$i]['toDate'] = datePars($rows[$i]['toDate']);
-        array_push($sortArr, $rows[$i]);
-    }
-}
+// for ($i=0; $i < count($rows); $i++) {
+//     if ($rows[$i]['level'] !== '0') {
+//         $rows[$i]['percent'] = (float)$rows[$i]['percent'];
+//         $rows[$i]['percent'] = round($rows[$i]['percent'], 2);
+//         $rows[$i]['percent'] *= 100;
+//         $rows[$i]['cost'] = (float)$rows[$i]['cost'];
+//         $rows[$i]['cost'] = round($rows[$i]['cost'], 2);
+//         $rows[$i]['actualCost'] = (float)$rows[$i]['actualCost'];
+//         $rows[$i]['actualCost'] = round($rows[$i]['actualCost'], 2);
+//         $rows[$i]['fromDate'] = datePars($rows[$i]['fromDate']);
+//         $rows[$i]['toDate'] = datePars($rows[$i]['toDate']);
+//         array_push($sortArr, $rows[$i]);
+//     }
+// }
 
-for ($i=0; $i < count($sortArr); $i++) {
-    if ($sortArr[$i]['level'] == '1') {
-        array_push($levelOne, $sortArr[$i]);
-    }
-}
+// for ($i=0; $i < count($sortArr); $i++) {
+//     if ($sortArr[$i]['level'] == '1') {
+//         array_push($levelOne, $sortArr[$i]);
+//     }
+// }
 
-for ($i=0; $i < count($sortArr); $i++) {
-    if ($sortArr[$i]['level'] == '2') {
-        array_push($levelTwo, $sortArr[$i]);
-    }
-}
+// for ($i=0; $i < count($sortArr); $i++) {
+//     if ($sortArr[$i]['level'] == '2') {
+//         array_push($levelTwo, $sortArr[$i]);
+//     }
+// }
 
-unset($rows);
-unset($sortArr);
-
+// unset($rows);
+// unset($sortArr);
+// !
 
 echo "<pre>";
 print_r(json_encode($levelOne));
